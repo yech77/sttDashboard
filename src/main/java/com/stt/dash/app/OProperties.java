f129bb17781f05d28747d16764738b98ca1b2052
yermi echeverria <yermi.echeverria@gmail.com>
2021-05-19 18:02:10 -0400

    Incluir el archivo de propiedades,

---
+++ src/main/java/com/stt/dash/app/OProperties.java
@@ -0,0 +1,45 @@
+package com.stt.dash.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("file:${stt.sttq.propertyfile}/attributes.properties")
+public class OProperties {
+    @Value("${mq.imqaddresslist}")
+    private String imqaddresslist;
+
+    @Value("${mq.origen}")
+    private String origen;
+
+    @Value("${mq.target}")
+    private String target;
+    /**/
+    @Value("${agenda.filepath.upload}")
+    private String agendaFilePathUpload;
+
+    @Value("${agenda.filepath.base}")
+    private String agendafilePathBase;
+
+    public String getImqaddresslist() {
+        return imqaddresslist;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public String getAgendaFilePathUpload() {
+        return agendaFilePathUpload;
+    }
+
+    public String getAgendafilePathBase() {
+        return agendafilePathBase;
+    }
+
+}
