e881bfa6a68f30e608efa8d0f1ff2263a784cbd4
yermi echeverria <yermi.echeverria@gmail.com>
2021-06-13 17:19:43 -0400

    Programar se enciende segun las validaciones.

--- src/main/java/com/stt/dash/ui/views/bulksms/events/BulkSmsReviewEvent.java
+++ src/main/java/com/stt/dash/ui/views/bulksms/events/BulkSmsReviewEvent.java
@@ -1,5 +1,6 @@
-package com.stt.dash.ui.views.bulksms;
+package com.stt.dash.ui.views.bulksms.events;
 
+import com.stt.dash.ui.views.bulksms.FileToSendEditor;
 import com.stt.dash.ui.views.orderedit.OrderEditor;
 import com.vaadin.flow.component.ComponentEvent;
 
