83fe20a1c05470542cd59b32b17f2f5c0a443e0c
yermi echeverria <yermi.echeverria@gmail.com>
2021-07-15 08:35:38 -0400

    Pantalla de busqueda se ven los campos a buscar.

--- frontend/src/views/smsview/sms-view.ts
+++ frontend/src/views/smsview/sms-view.ts
@@ -10,8 +10,56 @@ export class SmsView extends LitElement {
 
     render() {
         return html`
+            <style include="shared-styles">
+                :host {
+                    width: 100%;
+                    -webkit-overflow-scrolling: touch;
+                    overflow: auto;
+                }
+
+                .vaadin-board-cell {
+                    padding: var(--lumo-space-s);
+                }
+
+                *::-ms-backdrop,
+                .vaadin-board-cell {
+                    padding: 0;
+                }
+
+                .column-chart {
+                    box-shadow: 0 2px 5px 0 rgba(23, 68, 128, 0.1);
+                    border-radius: 4px;
+                    height: calc(20vh - 64px) !important;
+                    min-height: 150px;
+                }
+
+                #yearlySalesGraph {
+                    height: calc(30vh - 64px) !important;
+                    min-height: 200px;
+                }
+
+                #monthlyProductSplit,
+                #ordersGrid {
+                    border-radius: 4px;
+                    box-shadow: 0 2px 5px 0 rgba(23, 68, 128, 0.1);
+                    height: calc(40vh - 64px) !important;
+                    min-height: 355px;
+                }
+
+                vaadin-board-row.custom-board-row {
+                    --vaadin-board-width-medium: 1440px;
+                    --vaadin-board-width-small: 1024px;
+                }
+
+            </style>
             <vaadin-board>
-                <div id="header"></div>
+                <vaadin-row>
+                    <div id="firstline" align="center"></div>
+                    <div id="secondline"></div>
+                </vaadin-row>
+                <vaadi-row>
+                    <vaadin-grid></vaadin-grid>
+                </vaadi-row>
             </vaadin-board>
         `;
     }
