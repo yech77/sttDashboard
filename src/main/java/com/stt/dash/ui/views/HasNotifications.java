92e7390e6b6d2086837046aaae26d18a67c4816b
yech77 <itjoye@gmail.com>
2023-04-11 09:20:57 -0400

    CAmbio de notificaciones a TOP CENTER

--- src/main/java/com/stt/dash/ui/views/HasNotifications.java
+++ src/main/java/com/stt/dash/ui/views/HasNotifications.java
@@ -9,25 +9,24 @@ import com.stt.dash.ui.utils.BakeryConst;
 
 /**
  * Interface for views showing notifications to users
- *
  */
 public interface HasNotifications extends HasElement {
 
-	default void showNotification(String message) {
-		showNotification(message, false);
-	}
+    default void showNotification(String message) {
+        showNotification(message, false);
+    }
 
-	default void showNotification(String message, boolean persistent) {
-		if (persistent) {
-			Button close = new Button("Cerrar");
-			close.getElement().setAttribute("theme", "tertiary small error");
-			Notification notification = new Notification(new Text(message), close);
-			notification.setPosition(Position.BOTTOM_START);
-			notification.setDuration(0);
-			close.addClickListener(event -> notification.close());
-			notification.open();
-		} else {
-			Notification.show(message, BakeryConst.NOTIFICATION_DURATION, Position.BOTTOM_STRETCH);
-		}
-	}
+    default void showNotification(String message, boolean persistent) {
+        if (persistent) {
+            Button close = new Button("Cerrar");
+            close.getElement().setAttribute("theme", "tertiary small error");
+            Notification notification = new Notification(new Text(message), close);
+            notification.setPosition(Position.TOP_START);
+            notification.setDuration(0);
+            close.addClickListener(event -> notification.close());
+            notification.open();
+        } else {
+            Notification.show(message, BakeryConst.NOTIFICATION_DURATION, Position.TOP_CENTER);
+        }
+    }
 }
