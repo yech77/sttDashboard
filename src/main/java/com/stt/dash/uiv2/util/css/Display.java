4f927f1a5747d01b47f7301362f291da339795ea
yech77 <itjoye@gmail.com>
2023-04-13 08:14:24 -0400

    Agregar componentes base para los cambios Vaadin Flow.

---
+++ src/main/java/com/stt/dash/uiv2/util/css/Display.java
@@ -0,0 +1,16 @@
+package com.stt.dash.uiv2.util.css;
+
+public enum Display {
+
+    BLOCK("block"), INLINE("inline"), FLEX("flex"), INLINE_FLEX("inline-flex");
+
+    private String value;
+
+    Display(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
